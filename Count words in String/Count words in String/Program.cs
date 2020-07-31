7a70d2b6416ce4ad058188609dffd5accdb939c0
stefanstefanov930613 <68789985+stefanstefanov930613@users.noreply.github.com>
2020-07-31 16:23:12 +0200

    Count words in String

---
+++ Count words in String/Count words in String/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Reverse_A_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText(@"C:\Users\Chefo1993\Desktop\Words.txt");
+
+            int counter = 0;
+            int words = 1;
+
+            while (counter <= text.Length - 1)
+            {
+                if (text[counter] == ' ' || text[counter] == '\n' || text[counter] == '\t')
+                {
+                    words++;
+                }
+
+                counter++;
+            }
+
+            Console.WriteLine($"Words in the string are: {words}");
+        }
+
+
+    }
+}
